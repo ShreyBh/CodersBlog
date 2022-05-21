662e88fd61937a7ceab07c816e6fbc5fe2ff9c8d
ShreyBh <83309556+ShreyBh@users.noreply.github.com>
2022-05-21 21:29:06 +0530

    Daily update done

--- pages/blog.js
+++ pages/blog.js
@@ -2,6 +2,10 @@ import React from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link'
 
+// Step 1: Collect all the files from blogdata directory
+// Step 2: Iterate through and display them
+
+
 const Blog = () => {
   return <div className={styles.container}>
           <main className={styles.main}>
